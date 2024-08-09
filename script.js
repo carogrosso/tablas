d8077753a5c48df3df4f4e220399468fbcae9f4f
Caro <grossocrln@gmail.com>
2024-08-09 12:45:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+var numero_inicial = document.getElementById("numero") 
+var generar = document.getElementById("generar")
+var resultado = document.getElementById("resultado")
+var reiniciar = document.getElementById("reiniciar")
+
+generar.addEventListener("click", function(){
+  let numerito = numero_inicial.value
+  if (numerito == 0) {
+    swal({
+      title: "Ingresa un número válido",
+        text: "El numero tiene que ser igual o mayor a 1.",
+        type: "warning",
+        showConfirmButton: false,
+        // showCancelButton: true,
+        cancelButtonColor: '#d33',
+        // cancelButtonText: "Ok"
+      });
+    // alert("Ingresa un número válido")
+  } else {
+  for (var i = 1; i < 11; i++) {
+    let un_resultado = numerito + " x " + i + " = " + (numerito * i)
+    var crear_resultado = document.createElement('h3')
+    var cuenta = document.createTextNode(un_resultado)
+    crear_resultado.appendChild(cuenta)
+    resultado.appendChild(crear_resultado)
+    numero_inicial.setAttribute("disabled", "")
+    generar.classList.add("display-none")
+    reiniciar.classList.remove("display-none")
+    }
+  }
+})
+
+reiniciar.addEventListener("click", function(){
+  location.reload()
+})
